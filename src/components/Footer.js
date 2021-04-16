d00f07e700224624d32e6016dcbfcdf20abd249a
Zach Smith <znathanielsmith@gmail.com>
2021-04-15 21:19:28 -0400

    grapefruit typo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="content has-text-centered">
         <strong>Tasting Notes:</strong>
         <p>
-          Earthy, nutty, piney. Loads of citrus and grapefuit. Brewed by{" "}
+          Earthy, nutty, piney. Loads of citrus and grapefruit. Brewed by{" "}
           <a href="https://github.com/SmitharyZach">Zach Smith</a>. Inspired by{" "}
           <a href="https://gregavola.com/">Greg Avola</a> and the now defunct
           BeerMemories project.
